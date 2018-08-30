5b999c42849e457d3690f7de018a2a0a83fc93e3
klbennett <https://github.com/settings/applications>
2018-08-30 13:45:21 +0100

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,3 @@
-# #Shoe
-#   ::new
-#     gets initialized with a brand (FAILED - 1)
-#   properties
-#     has a brand (FAILED - 2)
-#     has a color (FAILED - 3)
-#     has a size (FAILED - 4)
-#     has a material (FAILED - 5)
-#     has a condition (FAILED - 6)
-#   #cobble
-#     says that the shoe has been repaired (FAILED - 7)
-#     makes the shoe's condition new (FAILED - 8)
-
 class Shoe
 
   def initialize(brand)
