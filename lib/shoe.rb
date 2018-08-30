5a9e5cd3a7359f52bfd53b73c8d9a1e614be00f5
klbennett <https://github.com/settings/applications>
2018-08-30 12:56:07 +0100

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,62 @@
-# Make your shoe class here!
+# #Shoe
+#   ::new
+#     gets initialized with a brand (FAILED - 1)
+#   properties
+#     has a brand (FAILED - 2)
+#     has a color (FAILED - 3)
+#     has a size (FAILED - 4)
+#     has a material (FAILED - 5)
+#     has a condition (FAILED - 6)
+#   #cobble
+#     says that the shoe has been repaired (FAILED - 7)
+#     makes the shoe's condition new (FAILED - 8)
+
+class Shoe
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def brand
+    @brand
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def color
+    @color
+  end
+
+  def size=(size)
+    @size = size
+  end
+
+  def size
+    @size
+  end
+
+  def material=(mat)
+    @material = mat
+  end
+
+  def material
+    @material
+  end
+
+  def condition=(condition)
+    @condition = condition
+  end
+
+  def condition
+    @condition
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+
+end
